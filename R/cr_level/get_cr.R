9d5672efae46cee15104bbea85f4386b43dbf2bd
idohasson <ido.hasson.5@gmail.com>
2022-10-13 22:07:49 +0300

    avarege cr level

--- R/cr_level/get_cr.R
+++ R/cr_level/get_cr.R
@@ -1,1 +1,23 @@
 
+cr_level.data.frame <- function(df) {
+  df %>%
+    distinct(sample, nSeqCDR3, aaSeqCDR3) %>%
+    with(table(aaSeqCDR3, sample))
+}
+
+avarege_cr_level.data.frame <- function(df) {
+  df %>%
+    cr_level.data.frame %>%
+    apply(MARGIN = 2, mean)
+}
+
+
+# group_field="sample"
+# nt_field="nSeqCDR3"
+# aa_field="aaSeqCDR3"
+# tbl=as_tibble(df)
+
+# tbl %>%
+#   distinct_at(.vars = c(group_field, nt_field, aa_field)) %>%
+#   select(aa_field, group_field) %>%
+#   table
