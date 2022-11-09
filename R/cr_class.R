7f4c888f90fc15101c29a403d1f831e7da020101
idohasson <ido.hasson.5@gmail.com>
2022-11-09 13:10:18 +0200

    classification

--- R/cr_class.R
+++ R/cr_class.R
@@ -1,5 +1,8 @@
 #' Sub-classification of public clonotype
 #'
+#' @description computes the CR-class of each clonotype by its frequency in each group of repertoires.
+#'
+#'
 #' @param group_count numerical vector / or multiple dimensional data-type (data frame / matrix / array) of number of colonotype in each group
 #' @param margin the dimension to apply
 #' @param min_freq minimal value per group
@@ -14,109 +17,71 @@
 #' df_lists <- replicate(4, rand_group(), FALSE)
 #' rand_df <- group_join(df_lists)
 #' tbl <- share_table(rand_df, "clonotype", "group", "rep_id")
-#' cr_class(tbl)
+#' cr_class(head(tbl, 5))
 #'
 cr_class <- function(group_count, margin=1, min_freq=1, public_min=2, exclusive_max=1) {
 
-  # computes the CR-class of each clonotype by its frequency in each group of repertoires.
-  compute_type <- function(x) {
-    # public clonotype    inclusive clonotype
-    i <- x >= min_freq
-    # can't be private    multiple shared samples
-    # (sum(x[i]) >= public_min) + (sum(i) <= exclusive_max)
-    (p <- sum(x[i]) >= public_min) + (p & sum(i) <= exclusive_max)
-    # private = 0 | exclusive = 1 | inclusive = 2
-  }
-
-  cr_index <-
-
-    if (is.vector(group_count)) # checking whether the input is a vector.
-      # If it is, then it’s a simple count and we can just use the compute_type function.
-      compute_type(group_count)
+  if (is.vector(group_count)) # checking whether the input is a vector.
+    # If it is, then it’s a simple count and we can just use the compute_type function.
+    cr_freq_class(group_count, min_freq, public_min, exclusive_max)
     # If the input is not a vector, then we need to check whether the object has dimensions (it’s a array / matrix / data frame).
-    else if (!is.null(dim(group_count)))
-      # If it is, then we need to apply the compute_type function to each row.
-      apply(group_count, margin, compute_type)
+  else if (!is.null(dim(group_count)))
+    # If it is, then we need to apply the compute_type function to each row.
+    apply(group_count, margin, cr_freq_class, min_freq, public_min, exclusive_max)
     # input is not a vector, matrix, or data frame, then we need to stop because we don’t know how to handle it.
-    else stop("")
+  else stop("Invalid input")
 
-  dplyr::case_when(
-    # checking if the cr_index is equal to 2.
-    cr_index==2 ~ "exclusive", # If so, it returns the string “exclusive”.
-    # checking if the cr_index is equal to 1.
-    cr_index==1 ~ "inclusive", # If so, it returns the string “inclusive”.
-    # checking if the cr_index is equal to 0.
-    cr_index==0 ~ "private", # If so, it returns the string “private”.
-    # checking if the cr_index is not equal to 2, 1, or 0.
-    TRUE ~ NA_character_ # If so, it returns the string “NA”.
-  )
-
-  # factor(cr_index,
-  #        levels = c(0, 1, 2),
-  #        # labels = c("private", "exclusive", "inclusive"))
-  #        labels = c("private", "inclusive", "exclusive"))
 }
 
-min_freq=1; public_min=2; exclusive_max=1
-compute_type <- function(x) {
-  # public clonotype    inclusive clonotype
-  i <- x >= min_freq
-  # can't be private    multiple shared samples
-  # (sum(x[i]) >= public_min) + (sum(i) <= exclusive_max)
-  (p <- sum(x[i]) >= public_min) + (p & sum(i) <= exclusive_max)
-  # private = 0 | exclusive = 1 | inclusive = 2
-}
+#' function that takes a vector of frequencies and returns a character vector of class labels.
+#'
+#' @describeIn
+#'
+#' given clonotype frequencies in each group, calculate its convergent
+#' recombination public classifications based on various conditions.
+#'
+#' @param c_freq numerical vector
+#' @param min_freq minimum numerical value
+#' @param public_min minimum sum from all frequencies having a minimal value to be considered as public clonotype
+#' @param exclusive_max maximal number of public clonotypes groups to be considered as public exclusive. o.w. inclusive.
+#'
+#' @return character
+#' @export
+#'
+#' @examples
+#'
+#' cr_freq_class(c(1,0,1,0)) # public inclusive
+#' cr_freq_class(c(2,0,0,0)) # public exclusive
+#' cr_freq_class(c(1,0,0,0)) # private
+#' cr_freq_class(c(0,0,0,0)) # NA if no clonotype exists
+#' cr_freq_class(c(1,2,0,10), min_freq=2) # public inclusive
+#' cr_freq_class(c(1,2,0,10), min_freq=3) # public exclusive
+#' cr_freq_class(c(1,2,0,10), min_freq=11) # also NA if non frequencies ai sufficient
+#' cr_freq_class(c(1,2,0,10), min_freq=2, exclusive_max=2) # public exclusive
+#' cr_freq_class(c(1,2,0,10), min_freq=1, exclusive_max=2) # public inclusive
+#' cr_freq_class(c(0, 1, 2, 3), min_freq=1, public_min=5) # public inclusive
+#' cr_freq_class(c(0, 1, 2, 4), min_freq=2, public_min=5) # public exclusive
+#' cr_freq_class(c(0, 1, 2, 4), min_freq=3, public_min=5) # private
+#'
+#'
+#'
+cr_freq_class <- function(c_freq, min_freq=1, public_min=2, exclusive_max=1) {
 
-# whole numbers
-# library("magrittr")
-f <- function(x) {
-  # if(!any(i <- (x >= min_freq & !is.na(x)), na.rm = TRUE)) return(NA)
-  # any(i <- (x >= min_freq & !is.na(x)), na.rm = TRUE)
-  # i <- x >= min_freq & !is.na(x)
-  i <- x >= min_freq
-
-  is.not.empty <- any(i, na.rm = TRUE)
-  is.public <- sum(x[i], na.rm = TRUE) >= public_min # FALSE is private
-  is.exclusive <- sum(i, na.rm = TRUE) <= exclusive_max # FALSE is inclusive
-
+  i_freq <- c_freq >= min_freq
+  # returns TRUE if the vector of frequencies has at least one non-missing value.
+  is.not.empty <- any(i_freq, na.rm = TRUE)
+  # returns TRUE if the sum of frequencies is greater than or equal to the minimum public frequency.
+  is.public <- sum(c_freq[i_freq], na.rm = TRUE) >= public_min
+  # returns TRUE if the number of groups having minimal frequency value is less than or equal to the maximum exclusive frequency.
+  is.exclusive <- sum(i_freq, na.rm = TRUE) <= exclusive_max
+  # return the appropriate class label based on the values in the previous lines.
   dplyr::case_when(
-    is.public & is.exclusive ~ "exclusive",
-    is.public ~ "inclusive",
+    is.public & is.exclusive ~ "public_exclusive",
+    is.public ~ "public_inclusive",
     is.not.empty ~ "private",
-    TRUE ~ NA_character_
+    TRUE ~ NA_character_ # returns NA if none of the above conditions are met.
   )
-
 }
-#
-# min_freq=2; public_min=2; exclusive_max=1
-# c(0,0,0,0) %>% f()
-# c(0,0,0,NA) %>% f()
-# c(NA,NA,NA,NA) %>% f()
-# c(0,1,0,0) %>% f()
-# c(0,1,0,NA) %>% f()
-# c(NA,1,NA,NA) %>% f()
-# c(0,1,0,1) %>% f()
-# c(NA,1,NA,1) %>% f()
-# c(0,0,0,2) %>% f()
-# c(0,NA,0,2) %>% f()
-# c(NA,NA,NA,2) %>% f()
-# c(0,0,1,2) %>% f()
-# # cr_class(min_freq=1, public_min=2, exclusive_max=1)
-# min_freq=.15; public_min=.2; exclusive_max=1
-# c(0,0,0,0) %>% f()
-# c(0,0,0,NA) %>% f()
-# c(NA,NA,NA,NA) %>% f()
-# c(0,.1,0,0) %>% f()
-# c(0,.1,0,NA) %>% f()
-# c(NA,.1,NA,NA) %>% f()
-# c(0,.1,0,.1) %>% f()
-# c(NA,.1,NA,.1) %>% f()
-# c(0,0,0,.2) %>% f()
-# c(0,NA,0,.2) %>% f()
-# c(NA,NA,NA,.2) %>% f()
-# c(0,0,.1,.2) %>% f()
-
-
 
 
 
