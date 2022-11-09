9500d995b460581e2b90ff553f5f04bea4e62498
ido hasson <ido.hasson.5@gmail.com>
2022-11-09 11:52:41 +0200

    classification fixed

--- R/cr_class.R
+++ R/cr_class.R
@@ -18,26 +18,37 @@
 #'
 cr_class <- function(group_count, margin=1, min_freq=1, public_min=2, exclusive_max=1) {
 
+  # computes the CR-class of each clonotype by its frequency in each group of repertoires.
   compute_type <- function(x) {
     # public clonotype    inclusive clonotype
-    # can't be private    multiple shared samples
     i <- x >= min_freq
+    # can't be private    multiple shared samples
     # (sum(x[i]) >= public_min) + (sum(i) <= exclusive_max)
     (p <- sum(x[i]) >= public_min) + (p & sum(i) <= exclusive_max)
     # private = 0 | exclusive = 1 | inclusive = 2
   }
 
-  cr_index <- if (is.vector(group_count))
-    compute_type(group_count)
-  else if (!is.null(dim(group_count)))
-    apply(group_count, margin, compute_type)
-  else stop("")
+  cr_index <-
 
-  case_when(
-    cr_index==2 ~ "exclusive",
-    cr_index==1 ~ "inclusive",
-    cr_index==0 ~ "private",
-    TRUE ~ NA_character_
+    if (is.vector(group_count)) # checking whether the input is a vector.
+      # If it is, then it’s a simple count and we can just use the compute_type function.
+      compute_type(group_count)
+    # If the input is not a vector, then we need to check whether the object has dimensions (it’s a array / matrix / data frame).
+    else if (!is.null(dim(group_count)))
+      # If it is, then we need to apply the compute_type function to each row.
+      apply(group_count, margin, compute_type)
+    # input is not a vector, matrix, or data frame, then we need to stop because we don’t know how to handle it.
+    else stop("")
+
+  dplyr::case_when(
+    # checking if the cr_index is equal to 2.
+    cr_index==2 ~ "exclusive", # If so, it returns the string “exclusive”.
+    # checking if the cr_index is equal to 1.
+    cr_index==1 ~ "inclusive", # If so, it returns the string “inclusive”.
+    # checking if the cr_index is equal to 0.
+    cr_index==0 ~ "private", # If so, it returns the string “private”.
+    # checking if the cr_index is not equal to 2, 1, or 0.
+    TRUE ~ NA_character_ # If so, it returns the string “NA”.
   )
 
   # factor(cr_index,
@@ -46,27 +57,82 @@ cr_class <- function(group_count, margin=1, min_freq=1, public_min=2, exclusive_
   #        labels = c("private", "inclusive", "exclusive"))
 }
 
-# sharing_samples <- rowSums(clonal_seq_share_level, na.rm = TRUE)
-# sharing_groups <- rowSums(clonal_seq_share_level != 0, na.rm = TRUE)
-#
-# is_private <- sharing_samples == 1
-# is_public <- sharing_samples > 1
-# is_exclusive <- is_public & (sharing_groups == 1)
-# is_inclusive <- is_public & (sharing_groups > 1)
-#
-# clone_sharing <- list(sample=sharing_samples,
-#                       group=sharing_groups)
-#
-# clone_cr_type <- list(private=is_private,
-#                       public=is_public,
-#                       exclusive=is_exclusive,
-#                       inclusive=is_inclusive)
-#
-#
-# share_ratio <- clonotype_sharing$sample/clonotype_sharing$group
-# clonotype_cr_ratio <- lapply(clonotype_cr_type, function(x) share_ratio[x])
-# print(sapply(clonotype_cr_ratio, mean))
-#
-# share_ratio <- clone_sharing$sample/clone_sharing$group
-# clone_cr_ratio <- lapply(clone_cr_type, function(x) share_ratio[x])
-# print(sapply(clone_cr_ratio, mean))
+min_freq=1; public_min=2; exclusive_max=1
+compute_type <- function(x) {
+  # public clonotype    inclusive clonotype
+  i <- x >= min_freq
+  # can't be private    multiple shared samples
+  # (sum(x[i]) >= public_min) + (sum(i) <= exclusive_max)
+  (p <- sum(x[i]) >= public_min) + (p & sum(i) <= exclusive_max)
+  # private = 0 | exclusive = 1 | inclusive = 2
+}
+
+# whole numbers
+# library("magrittr")
+f <- function(x) {
+  # if(!any(i <- (x >= min_freq & !is.na(x)), na.rm = TRUE)) return(NA)
+  # any(i <- (x >= min_freq & !is.na(x)), na.rm = TRUE)
+  # i <- x >= min_freq & !is.na(x)
+  i <- x >= min_freq
+
+  is.not.empty <- any(i, na.rm = TRUE)
+  is.public <- sum(x[i], na.rm = TRUE) >= public_min # FALSE is private
+  is.exclusive <- sum(i, na.rm = TRUE) <= exclusive_max # FALSE is inclusive
+
+  dplyr::case_when(
+    is.public & is.exclusive ~ "exclusive",
+    is.public ~ "inclusive",
+    is.not.empty ~ "private",
+    TRUE ~ NA_character_
+  )
+
+}
+
+min_freq=1; public_min=2; exclusive_max=1
+c(0,0,0,0) %>% f()
+c(0,0,0,NA) %>% f()
+c(NA,NA,NA,NA) %>% f()
+c(0,1,0,0) %>% f()
+c(0,1,0,NA) %>% f()
+c(NA,1,NA,NA) %>% f()
+c(0,1,0,1) %>% f()
+c(NA,1,NA,1) %>% f()
+c(0,0,0,2) %>% f()
+c(0,NA,0,2) %>% f()
+c(NA,NA,NA,2) %>% f()
+c(0,0,1,2) %>% f()
+# cr_class(min_freq=1, public_min=2, exclusive_max=1)
+min_freq=.1; public_min=.25; exclusive_max=1
+c(0,0,0,0) %>% f()
+c(0,0,0,NA) %>% f()
+c(NA,NA,NA,NA) %>% f()
+c(0,.1,0,0) %>% f()
+c(0,.1,0,NA) %>% f()
+c(NA,.1,NA,NA) %>% f()
+c(0,.1,0,.1) %>% f()
+c(NA,.1,NA,.1) %>% f()
+c(0,0,0,.2) %>% f()
+c(0,NA,0,.2) %>% f()
+c(NA,NA,NA,.2) %>% f()
+c(0,0,.1,.2) %>% f()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
