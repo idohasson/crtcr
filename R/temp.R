21c5f54cc253dd69bd899e95c6fc91b26feaaa62
idohasson <ido.hasson.5@gmail.com>
2022-12-03 21:25:31 +0200

    .

--- R/temp.R
+++ R/temp.R
@@ -1,6 +1,294 @@
 library(vctrs)
 library(rlang)
 
+cr_func <- function(..., .func=cr_level, .use_id=FALSE, .use_group=FALSE) {
+
+  if (.use_id) {
+
+    function(.id, ...) {
+
+      .func(.id, ...)
+
+    }
+
+  } else if (.use_group) {
+
+    function(.id, ...) {
+
+      clone_list <- vec_chop(..., indices = .id$loc)
+
+      vapply(clone_list, .func, numeric(1), ...)
+
+    }
+
+  } else {
+
+    .func(...)
+
+  }
+
+
+}
+
+apply_group <- function(x, ..., by_id=FALSE, .func) {
+
+  dfl <- df_list(..., .name_repair = "unique_quiet")
+
+  groups <- new_data_frame(dfl)
+
+  if (by_id) {
+
+    group_id <- vec_group_id(groups)
+
+    .func(x, group_id)
+
+  } else {
+
+    group_loc <- vec_group_loc(groups)
+
+    .func(x, group_loc)
+
+  }
+
+}
+
+
+cr_mean <- function(.nt) {
+  vec_unique_count(.nt) / attr(.nt, "n")
+}
+
+cr_number <- function(.nt) {
+  vec_unique_count(.nt)
+}
+
+cr_level <- function(.clone, ..., .cr_func=cr_number) {
+  vapply(.clone, .cr_func, numeric(1), ...)
+}
+
+
+
+
+nt_list <- vec_chop(.clone, indices = id$loc)
+
+apply_group(z,x,.func = cr_level)
+
+apply_group(z,x,.func = cr_level)
+
+my_func <- cr_func(.func = cr_mean)
+
+apply_group(z,x, by_id = TRUE, .func = my_func)
+
+
+
+
+group_id <- apply_group(x, y)
+cr_number(z)
+cr_level(z, group_id)
+
+# x <- sample(LETTERS[1:5], 30, rep=T)
+# y <- sample(letters[1:3], 30, rep=T)
+# z <- sample(replicate(20, rand_nt(3)), 30, rep=T)
+
+
+grp_loc <- vec_group_loc(groups)
+id <- vec_group_id(groups)
+
+z
+
+l <- vec_chop(z, groups$loc)
+
+vapply(l, vec_unique_count, integer(1))
+
+
+
+
+each_unique <- function(.data, .unique_by, ..., .each_func=n_unique) {
+
+  indices <- vec_seq_along(.unique_by)
+
+  unique_list <- vec_split(indices, by = .unique_by)
+
+  .each_func(.data, unique_list$val)
+
+}
+
+unique_freq <- function(.freq_of, .unique_by) {
+
+  freq_split <- vec_chop(.freq_of, .unique_by)
+
+  compute_freq <- lapply(freq_of, vec_unique_count)
+
+  list_unchop(compute_freq, indices = .unique_by)
+
+}
+
+unique_weight <- function(.weight, .unique_by) {
+
+  id <- vec_group_id(.weight)
+
+  id_list <- vec_chop(id, .unique_by)
+
+  vapply(id_list, function(z) vec_unique_count(z) / attr(z,"n"), numeric(1))
+
+}
+
+vec_count(x, sort = "location")
+
+x_l <- vapply(x_id$loc, length, numeric(1))
+
+y_w <- vapply(vec_chop(y, x_id$loc), vec_unique_count, numeric(1))
+
+x_l * y_w / length(x_id$key)
+
+
+
+
+y <- sample(LETTERS[1:20], 100, rep=T)
+
+x_id <- vec_group_loc(x)
+y_id <- vec_group_loc(y)
+
+x_id$key
+y_id$key
+
+x_id$loc
+y_id$loc
+
+
+
+each_unique(x, y, .each_func = unique_freq)
+
+each_unique(rep(1, length(x)), x, .each_func = unique_weight)
+
+
+freq <- unique_freq(x, ids$val)
+
+
+
+tapply(x, y, vec_unique_count)
+
+tapply(freq, y, mean)
+
+list_unchop(as.list(freq), indices = ids$val)
+
+tapply(freq, ids$key, mean)
+
+tapply(x,y,vec_unique_count)
+
+unique_freq(x,y)
+
+vec_count(x, sort = "location")
+
+
+# l <- vec_recycle_common(1, x)
+# uid <- vec_group_id(x)
+# uv <- each_unique(l[[1]], x)
+# uv[uid] / attr(uid, "n")
+
+weight_unique(x)
+weight_unique(x, .weigh = a)
+weight_unique(x, .weigh_unique = 2)
+weight_unique(x, b, .weigh_func = mean)
+
+weight_unique <- function(.data, .weigh=1, .weigh_unique=1, ..., .weigh_func=sum) {
+
+  .weigh <- vec_recycle_common(.weigh, .data)[[1]]
+
+  val_each_unique <- tapply(.weigh, .data, .weigh_func)
+
+  # .weigh_unique <- vec_recycle_common(.weigh_unique, val_each_unique)[[1]]
+  # val_each_unique <- val_each_unique * .weigh_unique / vec_size(val_each_unique)
+
+  val_each_unique <- val_each_unique / vec_size(val_each_unique)
+
+  val_each_unique[.data]
+}
+
+each_unique(x, y)
+
+weight_unique(x)
+vec_count(x)$count / 10
+weight_unique(x, .weigh_func = sum)
+weight_unique(x, .weigh_func = length)
+
+x2 <- weight_unique(x)
+each_unique(x2, y, .each_func = sd)
+each_unique(rep_len(1,length(x)), y, .each_func = mean)
+
+
+
+
+cr_number <- function(.clone, .id, ..., .cr_func=n_unique) {
+
+  .cr_func(.clone, ...)
+
+}
+
+share_number <- function(.id, .share_func=n_unique, ...) {
+
+  .share_func(.id, ...)
+
+}
+
+x <- sample(letters[1:10], 100, rep=T)
+y <- sample(LETTERS[1:10], 100, rep=T)
+
+a <- rpois(100, 2)
+b <- runif(100)
+c <- runif(100, min = -1)
+
+cr_number(c("a", "b", "a"))
+cr_number(c("a", "b", "a"), .cr_func = length)
+cr_number(c(1,0,1))
+cr_number(c(1,0,1), .cr_func = length)
+
+share_number(c("a", "b", "a"))
+share_number(c("a", "b", "a"), .share_func = length)
+share_number(c(1,0,1))
+share_number(c(1,0,1), .share_func = length)
+
+
+each_unique(x,y, .each_func = cr_number)
+
+each_unique(x,y)
+each_unique(y,x)
+each_unique(rep_along(x, 1), x)
+each_unique(rep_along(x, 1), x)
+
+
+cr_number(x, y)
+share_number(x, y)
+
+# in cr-level arg
+# .each_unique=NULL
+each_unique(x,y, cr_func)
+# in share-level arg
+# .each_unique=NULL
+each_unique(y,x, share_func)
+
+w=1;f=n_unique
+each_unique(x,y,f)
+
+
+
+
+v <- uv[x] / sum(uv)
+
+
+each_unique(l[[1]],x , .each_func = length) # "mean"/z-score/
+
+tapply(a, x, mean)
+tapply(b, x, sum)
+tapply(c, x, sd)
+
+
+
+share_number <- function(.clone, .rid, ...) {
+
+  tapply(.rid, list(.clone, ...), vec_unique_count)
+
+}
+
 share_number <- function(.clone, .rid, ...) {
 
   tapply(.rid, list(.clone, ...), vec_unique_count)
