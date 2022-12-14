9557595d5a94fecd938f30fc11b576a8c82c0045
idohasson <ido.hasson.5@gmail.com>
2022-12-14 16:43:47 +0200

    .

--- R/cr_level.R
+++ R/cr_level.R
@@ -5,6 +5,77 @@ library(tibble)
 # convergent_recombination_level <- function()
 # NT=rand_nt_vec(100, 1);AA=translate(NT);ID=rep_along(NT, 1:4);FREQ=runif(100);DF=data.frame(nt=NT, aa=AA, id=ID, freq=FREQ)
 
+# is_cr(c("ATC", "ATT"))
+is_cr <- function(nt) {
+
+
+
+  aa <- lapply(nt, translate)
+
+  Reduce(`==`, aa)
+
+}
+
+split_cr <- function(nt) {
+
+  aa <- translate(nt)
+
+  split(nt, aa)
+
+}
+
+unique_cr <- function(nt) {
+
+  aa <- translate(nt)
+
+  cr_list <- split(nt, aa)
+
+  lapply(cr_list, unique)
+
+}
+
+count_cr <- function(nt) {
+
+  aa <- translate(nt)
+
+  unique_aa <- unique(aa)
+
+  length(unique_aa)
+
+}
+
+
+translate <- function(nt_vec) {
+  # checks that each element in the vector is a DNA base.
+  stopifnot("All strings must have consecutive triplets of 'A','G','T','C' (uppercase)"=all(grepl(pattern = "^([AGTC]{3})+$", nt_vec)))
+  # function for converting a DNA character vector into an amino acid character vector.
+  nt_to_aa <- function(nt) {
+    # a vector of integers that correspond to the nucleotide characters.
+    base_i <- c(T=0, C=1, A=2, G=3)
+    # a vector of characters that correspond to the amino acid characters.
+    codon_talbe <- c("F", "F", "L", "L", "S", "S", "S", "S",
+                     "Y", "Y", "*", "*", "C", "C", "*", "W",
+                     "L", "L", "L", "L", "P", "P", "P", "P",
+                     "H", "H", "Q", "Q", "R", "R", "R", "R",
+                     "I", "I", "I", "M", "T", "T", "T", "T",
+                     "N", "N", "K", "K", "S", "S", "R", "R",
+                     "V", "V", "V", "V", "A", "A", "A", "A",
+                     "D", "D", "E", "E", "G", "G", "G", "G")
+
+    aa_index <- base_i[nt[seq(1,length(nt),3)]] * 16 +
+      base_i[nt[seq(2,length(nt),3)]] * 4 +
+      base_i[nt[seq(3,length(nt),3)]] + 1
+
+    paste(codon_talbe[aa_index], collapse="")
+
+  }
+
+  vapply(strsplit(nt_vec, NULL), nt_to_aa, character(1))
+}
+
+
+
+
 # cr_number(NT[2:5],NT[5:7])
 cr_number <- function(...) {
 
