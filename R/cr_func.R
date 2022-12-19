c36e29262e08d18c816a79e104f2fbfc1ed1c89f
ido hasson <ido.hasson.5@gmail.com>
2022-12-20 00:01:35 +0200

    .

--- R/cr_func.R
+++ R/cr_func.R
@@ -1,14 +1,45 @@
-sapply(rand_clone_list(), cr_prop)
-cr_prop <- function(nt) {
+# sapply(rand_clone_list(), cr_prop)
+cr_prop <- function(nt, ...) {
 
-  vec_unique_count(nt) / vec_size(nt)
+  vctrs::vec_unique_count(nt) / vctrs::vec_size(nt)
 
 }
 
-with(rand_rep_df(), cr_subset(nt, aa))
-cr_subset <- function(nt, aa) {
+# with(rand_rep_df(), cr_subset(nt, aa))
+cr_subset <- function(nt, i) {
 
+  vctrs::vec_slice(nt, i)
 
+}
 
+cr_cond <- function(nt, ..., condition=cr_prop(nt)>0) {
+  # sum(as.logical(nt), na.rm = TRUE)>1
+  condition_call <- substitute(condition)
+
+  val <- list(nt, ...)
+
+  eval(condition_call, val)
+
+  # val <- do.call(func, l)
+  # val
+}
+
+# vctrs::vec_as_location(i, n, aa)
+# x <- rand_nt_vec()
+cr_cond(nt=x, aa=translate(x), condition = cr_prop(nt)>.1)
+
+cr_subset(x, sample(100, 10))
+
+condition <- function(..., .condition) {
+
+  condition_call <- substitute(.condition)
+
+  dfl <- df_list(..., .name_repair = "minimal")
+
+  val <- new_data_frame(dfl)
+
+  i_cond <- eval(condition_call, val)
+
+  i_cond
 
 }
