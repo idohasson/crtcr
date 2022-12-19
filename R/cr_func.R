c403ea7b58a098da5e91ec8037a15ccd773e47da
idohasson <ido.hasson.5@gmail.com>
2022-12-20 01:50:46 +0200

    .

--- R/cr_func.R
+++ R/cr_func.R
@@ -1,16 +1,56 @@
-# sapply(rand_clone_list(), cr_prop)
-cr_prop <- function(nt, ...) {
+# x <- rand_nt_vec()
+l <- rand_nt_list()
+# df <- rand_rep_df()
+# tbl <- with(df, tapply(nt, list(clonotype=aa, repertoire=id, group=id2), cr_number))
 
-  vctrs::vec_unique_count(nt) / vctrs::vec_size(nt)
+cr_number(x)
+cr_number <- function(nt) {
+
+  if (rlang::is_list(nt))
+
+    nt <- rlang::squash_chr(nt)
+
+  vec_unique_count(nt)
+
+}
+# cr_prop(x)
+# cr_prop(l)
+cr_prop <- function(nt, val=cr_number(nt)) {
+
+  val / vctrs::vec_size(nt)
+
+}
+# cr_subset(nt, 4:7)
+# cr_subset(x, translate(x))
+cr_subset <- function(nt, id=seq_along(nt), func=cr_prop) {
+
+  nt_chopped <- cr_loc(nt, id)
+  func(nt_chopped)
+
+}
+# vec_as_location(-1, 3, dimnames(tbl))
+# cr_loc(c("E", "R", "K"), translate(x))
+cr_loc <- function(nt, id) {
+
+  i <- vec_group_loc(id)
+
+  vec_chop(nt, i$loc)
 
 }
 
-# with(rand_rep_df(), cr_subset(nt, aa))
-cr_subset <- function(nt, i) {
+cr_norm <- function(nt, index, id) {
 
-  vctrs::vec_slice(nt, i)
+  i <- vec_group_loc(translate(nt))
+
+  x1 <- vec_chop(x, i$loc)
+
+  x2 <- vec_chop(df$id, i$loc)
+
+  mapply(cr_subset, x1, x2)
 
 }
+
+# vec_as_location(x, vec_size(x), x)
 
 cr_cond <- function(nt, ..., condition=cr_prop(nt)>0) {
   # sum(as.logical(nt), na.rm = TRUE)>1
@@ -23,12 +63,8 @@ cr_cond <- function(nt, ..., condition=cr_prop(nt)>0) {
   # val <- do.call(func, l)
   # val
 }
-
 # vctrs::vec_as_location(i, n, aa)
 # x <- rand_nt_vec()
-cr_cond(nt=x, aa=translate(x), condition = cr_prop(nt)>.1)
-
-cr_subset(x, sample(100, 10))
 
 condition <- function(..., .condition) {
 
